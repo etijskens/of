3c59b70ec7276b4968ed6ecbd78d0d1b92c35a26
the-john-doe <john.doe@example.com>
2022-11-01 20:32:26 +0100

    version (0.1.2) -> (0.1.3)

--- of/__init__.py
+++ of/__init__.py
@@ -6,17 +6,21 @@ Package of
 
 Top-level package for of.
 """
+import os
+import click
+from pathlib import Path
+import subprocess
+import shutil
 
-import of.cli_ofrun
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
-nCoresPerNode = {
+NCORESPERNODE = {
     'leibniz' :  28
   , 'vaughan' :  64
   , 'hortense': 128
 }
 
-def walltime(hours):
+def walltimeStr(hours):
     """Convert hours to slurm wall time format HH:MM:SS
 
     :param float hours: walltime in hours.
@@ -62,3 +66,86 @@ mpirun -np {nCores} renumberMesh -parallel -overwrite
 mpirun -np {nCores} {openfoam_solver} -parallel >& {case_name}.log"""
     # print(script)
     return script.format(nNodes=nNodes, nCores=nCores, walltime=walltime, case_name=case_name, openfoam_solver=openfoam_solver)
+
+
+def run( case
+       , destination=''
+       , ncores=1, nnodes=1
+       , walltime=1
+       , overwrite=False
+       , submit=False
+       , verbosity=0
+       ):
+    """Command line interface ofrun.
+
+    Copy an OpenFOAM case and run it with a number of nodes or cores for performance evaluation.
+    Create and submit job.
+    """
+    #  verify that case path exists
+    case_path = Path(case)
+    if not case_path.exists():
+        raise FileNotFoundError(case)
+
+    if verbosity:
+        click.echo('case = '+click.style(f"{case}", fg='green'))
+
+    # determine destination path and verify
+    dest_path = case_path.parent if not destination else Path(destination)
+    if not dest_path.exists():
+        raise FileNotFoundError(dest_path)
+
+    if verbosity:
+        click.echo('dest = '+click.style(f"{dest_path}", fg='green'))
+
+    cluster = os.environ['VSC_INSTITUTE_CLUSTER']
+    if cluster in ('local',''):
+        cluster = 'local-machine'
+        nNodes = 1
+        nCoresPerNode = ncores
+    else:
+        nNodes = nnodes
+        nCoresPerNode = NCORESPERNODE[cluster] if nNodes > 1 else ncores
+
+    nCores = nCoresPerNode * nnodes
+
+    if verbosity:
+        click.echo('cluster = ' + click.style(f"{cluster}", fg='green'))
+        click.echo('nNodes  = ' + click.style(f"{nNodes}", fg='green'))
+        click.echo('nCoresPerNode = ' + click.style(f"{nCoresPerNode}", fg='green'))
+        click.echo('ncores  = ' + click.style(f"{nCores}", fg='green'))
+
+    case_name = case_path.name+'-{}x{}cores'.format(nnodes, nCoresPerNode)
+    dest_path = dest_path / case_name
+    if verbosity:
+        click.echo('case_name = ' + click.style(f"{case_name}", fg='green'))
+
+    if overwrite:
+        shutil.rmtree(dest_path, ignore_errors=True)
+    shutil.copytree(case_path, dest_path)
+
+    # jobscript
+    job_script = jobscript(
+        nNodes=nNodes
+      , nCores=nCores
+      , walltime=walltimeStr(walltime)
+      , case_name=case_name
+      , openfoam_solver='icoFoam'
+    )
+    script_path = dest_path / f'{case_name}.slurm'
+    if verbosity > 1:
+        line = 80 * '-'
+        print(line)
+        click.echo('jobscript = ' + click.style(f"{script_path}", fg='green'))
+        print(line)
+        click.secho(job_script, fg='green')
+        print(line)
+    with open( script_path, mode='w') as f:
+        f.write(job_script)
+
+    if submit:
+        cmd = ['sbatch', script_path.name]
+        print(f'  > {" ".join(cmd)}')
+        subprocess.run(cmd, cwd=dest_path)
+        click.echo(click.style(f"Job script '{script_path}' submitted in directory '{dest_path}'.", fg='green'))
+    else:
+        click.echo(click.style(f"Job script '{script_path}' not submitted.", fg='red'))
--- of/cli_ofrun.py
+++ of/cli_ofrun.py
@@ -5,9 +5,6 @@ import sys,os
 sys.path.insert(0,'.')
 
 import click
-from pathlib import Path
-import subprocess
-import shutil
 
 import of
 
@@ -54,75 +51,13 @@ def main( case, destination
     Copy an OpenFOAM case and run it with a number of nodes or cores for performance evaluation.
     Create and submit job.
     """
-
-    #  verify that case path exists
-    case_path = Path(case)
-    if not case_path.exists():
-        raise FileNotFoundError(case)
-
-    if verbosity:
-        click.echo('case = '+click.style(f"{case}", fg='green'))
-
-    # determine destination path and verify
-    dest_path = case_path.parent if not destination else Path(destination)
-    if not dest_path.exists():
-        raise FileNotFoundError(dest_path)
-
-    if verbosity:
-        click.echo('dest = '+click.style(f"{dest_path}", fg='green'))
-
-    cluster = os.environ['VSC_INSTITUTE_CLUSTER']
-    if cluster in ('local',''):
-        cluster = 'local-machine'
-        nNodes = 1
-        nCoresPerNode = ncores
-    else:
-        nNodes = nnodes
-        nCoresPerNode = of.nCoresPerNode[cluster] if nNodes > 1 else ncores
-
-    nCores = nCoresPerNode * nnodes
-
-    if verbosity:
-        click.echo('cluster = ' + click.style(f"{cluster}", fg='green'))
-        click.echo('nNodes  = ' + click.style(f"{nNodes}", fg='green'))
-        click.echo('nCoresPerNode = ' + click.style(f"{nCoresPerNode}", fg='green'))
-        click.echo('ncores  = ' + click.style(f"{nCores}", fg='green'))
-
-    case_name = case_path.name+'-{}x{}cores'.format(nnodes, nCoresPerNode)
-    dest_path = dest_path / case_name
-    if verbosity:
-        click.echo('case_name = ' + click.style(f"{case_name}", fg='green'))
-
-    if overwrite:
-        shutil.rmtree(dest_path, ignore_errors=True)
-    shutil.copytree(case_path, dest_path)
-
-    # jobscript
-    jobscript = of.jobscript(
-        nNodes=nNodes
-      , nCores=nCores
-      , walltime=of.walltime(walltime)
-      , case_name=case_name
-      , openfoam_solver='icoFoam'
-    )
-    script_path = dest_path / f'{case_name}.slurm'
-    if verbosity > 1:
-        line = 80 * '-'
-        print(line)
-        click.echo('jobscript = ' + click.style(f"{script_path}", fg='green'))
-        print(line)
-        click.secho(jobscript, fg='green')
-        print(line)
-    with open( script_path, mode='w') as f:
-        f.write(jobscript)
-
-    if submit:
-        cmd = ['sbatch', script_path.name]
-        print(f'  > {" ".join(cmd)}')
-        subprocess.run(cmd, cwd=dest_path)
-        click.echo(click.style(f"Job script '{script_path}' submitted in directory '{dest_path}'.", fg='green'))
-    else:
-        click.echo(click.style(f"Job script '{script_path}' not submitted.", fg='red'))
+    of.ofrun( case, destination
+            , ncores, nnodes
+            , walltime
+            , overwrite
+            , submit
+            , verbosity
+            )
 
 
 if __name__ == "__main__":
